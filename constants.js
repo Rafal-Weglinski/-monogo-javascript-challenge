f0d160c82a28b057f3836a3fd25481eeeae717b4
Rafal-Weglinski <rafal.weglinski95@gmail.com>
2022-04-28 18:58:36 +0200

    add constants values used in the project

---
+++ constants.js
@@ -0,0 +1,7 @@
+const API_URL = 'https://www.monogo.pl/competition/input.txt'
+const COMPANY_NAME = 'Monogo'
+const BUILDING_NUMBER = 14
+
+exports.API_URL = API_URL
+exports.COMPANY_NAME = COMPANY_NAME
+exports.BUILDING_NUMBER = BUILDING_NUMBER
